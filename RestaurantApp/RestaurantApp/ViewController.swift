4692bd1e187dd3cac012225fe52b1b223d771590
Dastan <dastan211298@gmail.com>
2019-11-28 14:27:25 +0600

    restapp

---
+++ RestaurantApp/RestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Сейткасым Байтурсынов on 11/18/19.
+//  Copyright © 2019 Сейткасым Байтурсынов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
